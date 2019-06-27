ac2249a2a6e7362ebe8d20f5f57ac87c5e136aff
jonithereactdev <jonithereactdev@gmail.com>
2019-06-27 22:06:04 +0800

    UI created

---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class TodoItem extends Component {
+  //  state={  };
+
+  render() {
+    return (
+      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+        <h6>title</h6>
+        <div className="todo-icon">
+          <span className="mx-2 text-success">
+            <i className="fax fa-pen" />
+          </span>
+          <span className="mx-2 text-danger">
+            <i className="fas fa-trash" />
+          </span>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default TodoItem;
