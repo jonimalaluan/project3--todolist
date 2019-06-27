58bedc90ecfac006f3821c56d83cdaf09c5d51f3
jonithereactdev <jonithereactdev@gmail.com>
2019-06-28 00:12:54 +0800

    activate adding items

--- src/App.js
+++ src/App.js
@@ -5,7 +5,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import uuid from "uuid";
 
 class App extends Component {
-  //  state={  };
+  state = {
+    items: [],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false
+    });
+  };
 
   render() {
     return (
@@ -13,8 +41,12 @@ class App extends Component {
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-capitalize text-center">todo input</h3>
-            <TodoInput />
-            <TodoList />
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+            <TodoList items={this.state.items} />
           </div>
         </div>
       </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
-  //  state={  };
-
   render() {
+    const { items } = this.props;
     return (
       <ul className="list-group my-5">
         <h3 className="text-capitalize text-center">todo list</h3>
+        {items.map(item => {
+          return <TodoItem key={item.id} title={item.title} />;
+        })}
         <TodoItem />
         <button
           type="button"
